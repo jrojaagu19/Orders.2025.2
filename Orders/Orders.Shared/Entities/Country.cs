b5a59ffb25c70711990f5c34b23fb144c313b2d3
JUAN DIEGO ROJAS AGUDELO <juanrojas252034@correo.itm.edu.co>
2025-09-14 15:09:12 -0500

    Add countries emtity

---
+++ Orders/Orders.Shared/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Orders.Shared.Entities;
+
+public class Country
+{
+    public int Id { get; set; }
+
+    [Display(Name = "Pais")]
+    [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    public string Name { get; set; } = null!;
+}
